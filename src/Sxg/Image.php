fd6c7cf4a7e57a1bc0f7c692e47bd9fd5d3daacb
dmitri ponomarjov <dmitri@artweb.ee>
2017-02-28 21:25:04 +0200

    now supports: palette detection 256 colors new API

--- src/Sxg/Image.php
+++ src/Sxg/Image.php
@@ -11,25 +11,25 @@ class Image
     protected $version = 2;
     protected $backgroundColor = 0;
     protected $packingType = 0;
-    protected $rgbPalette;
     protected $paletteType = self::SXG_PALETTE_FORMAT_PWM;
 
-    protected $splittedRgbPalette;
+    protected $rgbPalette;
+    protected $sxgPalette;
     protected $pixels;
-    protected $colorFormat = 1;
-    protected $width = 320;
-    protected $height = 240;
+    protected $colorFormat = self::SXG_COLOR_FORMAT_16;
+    protected $width;
+    protected $height;
     protected static $clut = [
-        0  => 0,
-        1  => 10,
-        2  => 21,
-        3  => 31,
-        4  => 42,
-        5  => 53,
-        6  => 63,
-        7  => 74,
-        8  => 85,
-        9  => 95,
+        0 => 0,
+        1 => 10,
+        2 => 21,
+        3 => 31,
+        4 => 42,
+        5 => 53,
+        6 => 63,
+        7 => 74,
+        8 => 85,
+        9 => 95,
         10 => 106,
         11 => 117,
         12 => 127,
@@ -56,27 +56,11 @@ class Image
     }
 
     /**
-     * @return int
-     */
-    public function getHeight()
-    {
-        return $this->height;
-    }
-
-    /**
      * @param int $height
      */
     public function setHeight($height)
     {
         $this->height = $height;
-    }
-
-    /**
-     * @return int
-     */
-    public function getWidth()
-    {
-        return $this->width;
     }
 
     /**
@@ -124,7 +108,7 @@ class Image
      */
     public function getRgbPalette()
     {
-        return $this->rgbPalette;
+        return null;
     }
 
     /**
@@ -133,57 +117,73 @@ class Image
      */
     public function setRgbPalette($palette)
     {
-        $this->rgbPalette = $palette;
-        $this->splittedRgbPalette = [];
-        foreach ($this->rgbPalette as $color) {
-            $this->splittedRgbPalette[] = [($color >> 16) & 0xFF, ($color >> 8) & 0xFF, $color & 0xFF, $color];
+        $this->rgbPalette = [];
+        foreach ($palette as $color) {
+            $this->rgbPalette[] = [($color >> 16) & 0xFF, ($color >> 8) & 0xFF, $color & 0xFF, $color];
         }
         return $this;
     }
 
     public function importFromGd($gdObject)
     {
-        if ($gdObject = $this->convertGdObject($gdObject)) {
-            $this->readPixelsFromGd($gdObject);
-        }
-    }
-
-    protected function readPixelsFromGd($gdObject)
-    {
-        for ($y = 0; $y < $this->height; $y++) {
-            for ($x = 0; $x < $this->width; $x++) {
-                $this->pixels[] = imagecolorat($gdObject, $x, $y);
+        if ($gdObject = $this->resizeGdObject($gdObject)) {
+            if ($gdObject = $this->applyPaletteToGdObject($gdObject)) {
+                $this->importGdObject($gdObject);
+                return true;
             }
         }
+        return false;
     }
 
-    protected function convertGdObject($gdObject)
+    protected function resizeGdObject($gdObject)
     {
-        //create new object with restricted palette
-        $newObject = imagecreate($this->width, $this->height);
-        $width = imagesx($gdObject);
-        $height = imagesy($gdObject);
-
-        //create separate resource for palette holding.
-        //this cannot be created in $newObject unfortunately due to GD conversion bug,
-        //so we have to use intermediate object
-        $palette = imagecreate($this->width, $this->height);
-
-        //assign sxg palette colors to palette holding resource
-        foreach ($this->splittedRgbPalette as $color) {
-            imagecolorallocate($palette, $color[0], $color[1], $color[2]);
+        $sourceGdWidth = imagesx($gdObject);
+        $sourceGdHeight = imagesy($gdObject);
+        if (!$this->width) {
+            $this->width = $sourceGdWidth;
         }
+        if (!$this->height) {
+            $this->height = $sourceGdHeight;
+        }
+        $newObject = imagecreatetruecolor($this->width, $this->height);
+        imagecopyresampled($newObject, $gdObject, 0, 0, 0, 0, $this->width, $this->height, $sourceGdWidth, $sourceGdHeight);
+        return $newObject;
+    }
 
-        //here is the trick: assign palette before copying
-        imagepalettecopy($newObject, $palette);
-        //copy truecolor source to our new object with resampling. colors get replaced with palette as well.
-        imagecopyresampled($newObject, $gdObject, 0, 0, 0, 0, $this->width, $this->height, $width, $height);
-        //trick: assign palette after copying as well
-        imagepalettecopy($newObject, $palette);
+    protected function applyPaletteToGdObject($gdObject)
+    {
+        if ($this->rgbPalette) {
+            //create new object with restricted palette
+            $newObject = imagecreate($this->width, $this->height);
 
-        //        header('Content-type: image/png');
-        //        imagepng($newObject);
-        //        exit;
+            //create separate resource for palette holding.
+            //this cannot be created in $newObject unfortunately due to GD conversion bug,
+            //so we have to use intermediate object
+            $paletteResource = imagecreate($this->width, $this->height);
+
+            //assign sxg palette colors to palette holding resource
+            foreach ($this->rgbPalette as $color) {
+                imagecolorallocate($paletteResource, $color[0], $color[1], $color[2]);
+            }
+
+            //here is the trick: assign palette before copying
+            imagepalettecopy($newObject, $paletteResource);
+
+            //copy truecolor source to our new object with resampling. colors get replaced with palette as well.
+            imagecopy($newObject, $gdObject, 0, 0, 0, 0, $this->width, $this->height);
+
+            //trick: assign palette after copying as well
+            imagepalettecopy($newObject, $paletteResource);
+        } else {
+            $newObject = imagecreatetruecolor($this->width, $this->height);
+            imagecopy($newObject, $gdObject, 0, 0, 0, 0, $this->width, $this->height);
+            if ($this->colorFormat == self::SXG_COLOR_FORMAT_16) {
+                imagetruecolortopalette($newObject, false, 16);
+            } elseif ($this->colorFormat == self::SXG_COLOR_FORMAT_256) {
+                imagetruecolortopalette($newObject, false, 256);
+            }
+            imagecolormatch($gdObject, $newObject);
+        }
 
         return $newObject;
     }
@@ -195,23 +195,23 @@ class Image
 
     protected function generateSxgData()
     {
-        //        +#0000 #04 #7f+"SXG" - 4 байта сигнатура, что это формат файла SXG
-        //        +#0004 #01 1 байт версия формата
-        //        +#0005 #01 1 байт цвет фона (используется для очистки)
-        //        +#0006 #01 1 байт тип упаковки данных (#00 - данные не пакованы)
-        //        +#0007 #01 1 байт формат изображения (1 - 16ц, 2 - 256ц)
-        //        +#0008 #02 2 байта ширина изображения
-        //        +#000a #02 2 байта высота изображения
+        //        +#0000 #04 #7f+"SXG" - 4 bytes signature
+        //        +#0004 #01 1 byte of format version
+        //        +#0005 #01 1 byte of background color (used for non-fullscreen images)
+        //        +#0006 #01 1 byte of packing type (#00 - non-packed)
+        //        +#0007 #01 1 byte of format (1 - 16c, 2 - 256c)
+        //        +#0008 #02 2 bytes of width in pixels
+        //        +#000a #02 2 bytes of height in pixels
         //
-        //        (далее указываются смещения, для того, что бы можно было расширить заголовок)
-        //        +#000c #02 смещение от текущего адреса до начала данных палитры
-        //        +#000e #02 смещение от текущего адреса до начала данных битмап
+        //        (РґР°Р»РµРµ СѓРєР°Р·С‹РІР°СЋС‚СЃСЏ СЃРјРµС‰РµРЅРёСЏ, РґР»СЏ С‚РѕРіРѕ, С‡С‚Рѕ Р±С‹ РјРѕР¶РЅРѕ Р±С‹Р»Рѕ СЂР°СЃС€РёСЂРёС‚СЊ Р·Р°РіРѕР»РѕРІРѕРє)
+        //        +#000c #02 2 bytes of shifting from current address until palette data
+        //        +#000e #02 2 bytes of shifting from current address until pixels data
         //
-        //        Собственно начало данных палитры
-        //        +#0010 #0200 512 байт палитра
+        //        Palette start
+        //        +#0010 #0200 512 bytes of palette
         //
-        //        и начало данных битмап
-        //        +#0210 #xxxx данные битмап
+        //        Pixels start (bitmap data)
+        //        +#0210 #xxxx bitmap data
 
         $sxgPalette = $this->getSxgPalette();
         $sxgPixels = $this->getSxgPixels();
@@ -274,17 +274,41 @@ class Image
 
     public function getSxgPalette()
     {
-        $sxgPalette = [];
-        if ($this->paletteType == self::SXG_PALETTE_FORMAT_PWM) {
-            foreach ($this->splittedRgbPalette as $color) {
-                $sxgPalette[] = ($color[0] >> 3 << 10) + ($color[1] >> 3 << 5) + ($color[2] >> 3) + 32768;
-            }
-        } elseif ($this->paletteType == self::SXG_PALETTE_FORMAT_CLUT) {
-            foreach ($this->splittedRgbPalette as $color) {
-                $sxgPalette[] = ($this->findClosestClutValue($color[0]) << 10) + ($this->findClosestClutValue($color[1]) << 5) + $this->findClosestClutValue($color[2]);
+        if (($this->sxgPalette === null) && $this->rgbPalette) {
+            $this->sxgPalette = [];
+            if ($this->paletteType == self::SXG_PALETTE_FORMAT_PWM) {
+                foreach ($this->rgbPalette as $color) {
+                    $this->sxgPalette[] = ($color[0] >> 3 << 10) + ($color[1] >> 3 << 5) + ($color[2] >> 3) + 32768;
+                }
+            } elseif ($this->paletteType == self::SXG_PALETTE_FORMAT_CLUT) {
+                foreach ($this->rgbPalette as $color) {
+                    $this->sxgPalette[] = ($this->findClosestClutValue($color[0]) << 10) + ($this->findClosestClutValue($color[1]) << 5) + $this->findClosestClutValue($color[2]);
+                }
             }
         }
-        return $sxgPalette;
+        return $this->sxgPalette;
+    }
+
+    protected function importGdObject($gd)
+    {
+        if ($this->colorFormat == self::SXG_COLOR_FORMAT_16) {
+            $this->rgbPalette = array_fill(0, 16, null);
+        } elseif ($this->colorFormat == self::SXG_COLOR_FORMAT_256) {
+            $this->rgbPalette = array_fill(0, 256, null);
+        }
+        $this->pixels = [];
+        for ($y = 0; $y < $this->height; $y++) {
+            for ($x = 0; $x < $this->width; $x++) {
+
+                $index = imagecolorat($gd, $x, $y);
+                $this->pixels[] = $index;
+                if ($this->rgbPalette[$index] === null) {
+                    $color = imagecolorsforindex($gd, $index);
+                    $this->rgbPalette[$index] = [$color['red'], $color['green'], $color['blue']];
+                }
+            }
+        }
+        ksort($this->rgbPalette);
     }
 
     protected function findClosestClutValue($colorByte)
